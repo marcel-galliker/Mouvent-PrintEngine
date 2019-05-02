6d1ff98c7f687e8083ed67a65074c01176f017a0
maximilianE <maximilian.epple@mouvent.com>
2019-05-02 08:44:15 +0200

    mag_7139 embedded version numbers

--- rx_encoder_ctrl/version.c
+++ rx_encoder_ctrl/version.c
@@ -11,4 +11,4 @@
 
 #include "../rx_common_lib/version.h"
 
-const char version[32]="1.0.1.555";
+const char version[32]="1.0.1.556";
--- rx_stepper_ctrl/version.c
+++ rx_stepper_ctrl/version.c
@@ -12,7 +12,7 @@
 #include "../rx_common_lib/version.h"
 #include "string.h"
 
-const char version[32]="1.0.1.916";
+const char version[32]="1.0.1.921";
 
 void get_version(int ver[4])
 {
