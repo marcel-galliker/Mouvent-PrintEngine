a521fdff535cc430374823a4be02b11184ac7602
Michel Massaro <michel.massaro@mouvent.com>
2019-05-21 15:27:28 +0200

    Automated Build and Deploy

--- automated_build/deploy-mouventprinter.ps1
+++ automated_build/deploy-mouventprinter.ps1
@@ -9,7 +9,7 @@ param(
 Write-Host "targetpath = $targetpath"
 Write-Host "committag = $committag"
 Write-Host "buildname = $buildname"
-Write-Host "commitrefslug = $commitrfslug"
+Write-Host "commitrefslug = $commitrefslug"
 Write-Host "commitsh = $commitsha"
 
 $datestr=get-date -format "yyyyMMdd-HHmm"
@@ -29,7 +29,3 @@ else
 
 Write-Host "deploying bin to $targetpath\$committype.zip"
 7z.exe a $targetpath\$committype.zip bin
-
-#Compress-Archive -Path $InstallerPath\Installer\* -Update -DestinationPath $InstallerPath\Installer\Release_$version.zip
-#Send the archive in the SharePoint
-#Copy-Item "$InstallerPath\Installer\Release_$version.zip" "C:\Users\mouvent\MOUVENT AG\Workflow solutions - GitLab\Release $version.zip"
