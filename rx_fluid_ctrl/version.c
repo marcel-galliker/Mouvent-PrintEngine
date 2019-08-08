aaf317e72d48f7ec6ab6d145a024d96a5f688805
maximilianE <maximilian.epple@mouvent.com>
2019-08-08 10:33:58 +0200

    mag_7703 embedded version numbers

--- rx_fluid_ctrl/version.c
+++ rx_fluid_ctrl/version.c
@@ -11,4 +11,4 @@
 
 #include "../rx_common_lib/version.h"
 
-const char version[32]="1.0.1.515";
+const char version[32]="1.0.1.569";
--- rx_fluid_nios/software/rx_fluid_nios/version.c
+++ rx_fluid_nios/software/rx_fluid_nios/version.c
@@ -12,7 +12,7 @@
 #include "version.h"
 #include "string.h"
 
-const char version[32]="1.0.1.624";
+const char version[32]="1.0.1.674";
 
 void get_version(SVersion *pversion)
 {
