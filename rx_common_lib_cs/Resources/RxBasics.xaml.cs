488fdb029beaf7bf6525142ff1e169dd678817ad
maximilianE <maximilian.epple@mouvent.com>
2019-10-31 14:53:02 +0100

    mag_8234 code
    Version for Reynders
    new rx_encoder_rbf v579

--- rx_common_lib_cs/Resources/RxBasics.xaml.cs
+++ rx_common_lib_cs/Resources/RxBasics.xaml.cs
@@ -11,12 +11,14 @@ namespace RX_Common.Resources
             RxScreen screen=new RxScreen();
             if (screen.Surface)
             {
+                this["FontSizeXS"]    = 10.0;   
                 this["FontSize"]      = 20.0;   
                 this["FontSizeLarge"] = 25.0;   
                 this["FontSizeXL"]    = 40.0;   
             }
             else
             {
+                this["FontSizeXS"]    = 8.0;   
                 this["FontSize"]      = 14.66;            
                 this["FontSizeLarge"] = 20.0;   
                 this["FontSizeXL"]    = 32.0;   
