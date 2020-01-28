40d3ca308a68fdd7596631a13ed4d7a01cbd388f
Marcel Galliker <marcel.galliker@mouvent.com>
2020-01-28 15:07:37 +0100

    TX801: Change in PLC interface

--- rx_main_ctrl/plc_ctrl.c
+++ rx_main_ctrl/plc_ctrl.c
@@ -864,7 +864,6 @@ static void _plc_set_var(RX_SOCKET socket, char *varList)
 	char *val;
 	char *var=name;
 	int ret;
-	int toWebin=FALSE;
 	name[0]=0;
 
 //	TrPrintf(TRUE, "_plc_set_var");
@@ -880,25 +879,10 @@ static void _plc_set_var(RX_SOCKET socket, char *varList)
 			for(char *ch=val; *ch; ch++) if (*ch==',') *ch='.';
 			if (!_plc_set_cpu_cmd(name, val))
 			{
-				if (!strcmp(var, "CMD_SET_PARAMETER") && RX_PrinterStatus.printState==ps_webin && RX_Config.printer.type!=printer_cleaf)
-				{
-					Error(LOG, 0, "_plc_set_var: send CMD_STOP");
-					_plc_set_command("CMD_PRODUCTION", "CMD_STOP");
-					rx_sleep(200);
-					toWebin = TRUE;
-				}
 				ret = lc_set_value_by_name(name, val);
-				//---------------------------------------------------
-
 			//	Error(LOG, 0, "SET %s=%s", name, val);
 			//	TrPrintfL(TRUE, 0, "SET %s=%s", name, val);
-			//	TrPrintfL(!strcmp(name, "Application.GUI_00_001_Main.PAR_FLEXO_CONFIGURATION"), "_plc_set_var socket=%d >>%s=%s<< ret=%d", socket, name, val, ret);
 				if (ret)  ErrorFlag(ERR_CONT, &_ErrorFlags, 1, 0, "Writing >>%s=%s<<: Error %s", name, val, mlpi_get_errmsg());
-				if (toWebin)
-				{
-					rx_sleep(200);
-					_plc_set_command("CMD_SETUP", "CMD_WEBIN");	
-				}
 			}
 						
 			//--- XML_STEPPER_PRINT_HEIGHT special -----------------------------------------------
