471733999b12f408fbd65a23ff769b546c7f5e63
Marcel Galliker <marcel.galliker@mouvent.com>
2020-02-03 18:22:56 +0100

    Removed Eclipse files

--- rx_fluid_nios/software/rx_fluid_nios/obj/default/comm.d
+++
@@ -1,8 +0,0 @@
-obj/default/comm.o: comm.c comm.h rx_ink_common.h \
- ../rx_fluid_nios_bsp//HAL/inc/alt_types.h
-
-comm.h:
-
-rx_ink_common.h:
-
-../rx_fluid_nios_bsp//HAL/inc/alt_types.h:
--- rx_fluid_nios/software/rx_fluid_nios/version.c
+++ rx_fluid_nios/software/rx_fluid_nios/version.c
@@ -12,7 +12,7 @@
 #include "version.h"
 #include "string.h"
 
-const char version[32]="1.0.1.1052";
+const char version[32]="1.0.1.1053";
 
 void get_version(SVersion *pversion)
 {
