befefe75e3f0df6c6a6310f13c546c2c9ad5eb9e
Marcel Galliker <marcel.galliker@mouvent.com>
2020-07-22 09:20:22 +0200

    Added missing file

---
+++ mvt_digiprint_gui/Views/AlignmentView/SoftwareValueCorrectionView.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System.Windows;
+using RX_DigiPrint.Helpers;
+using RX_DigiPrint.Models;
+
+namespace RX_DigiPrint.Views.Alignment
+{
+    /// <summary>
+    /// Interaction logic for HeadAdjustmentView.xaml
+    /// </summary>
+    public partial class SoftwareValueCorrectionView : CustomWindow
+    {
+        private SoftwareValueCorrection _SoftwareValueCorrection;
+
+        public SoftwareValueCorrectionView(SoftwareValueCorrection.SoftwareValueType valueType, double recommendedValue, int globalHeadNumber)
+        {
+            InitializeComponent();
+
+            _SoftwareValueCorrection = new SoftwareValueCorrection(valueType, recommendedValue);
+
+            DataContext = _SoftwareValueCorrection;
+            if (globalHeadNumber>=0) TX_HeadName.DataContext = RxGlobals.HeadStat.List[globalHeadNumber];
+        }
+
+        private void SelectionLeftButton_Click(object sender, RoutedEventArgs e)
+        {
+            _SoftwareValueCorrection.LeftCorrectionImageSelected();
+        }
+
+        private void SelectionRightButton_Click(object sender, RoutedEventArgs e)
+        {
+            _SoftwareValueCorrection.RightCorrectionImageSelected();
+        }
+        
+        private void OkayButton_Click(object sender, RoutedEventArgs e)
+        {
+            DialogResult = true;
+        }
+
+        private void GoBackButton_Click(object sender, RoutedEventArgs e)
+        {
+            _SoftwareValueCorrection.ShowSelection();
+        }
+
+        private void Cancel_Click(object sender, RoutedEventArgs e)
+        {
+            DialogResult = false;
+        }
+
+        public double GetCorrectionValue()
+        {
+            return  _SoftwareValueCorrection.CorrectionValue.Correction;
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            RxGlobals.Screen.PlaceWindow(this);
+        }
+    }
+
+}
