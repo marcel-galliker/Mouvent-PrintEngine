2500bb75fc2265626f86b99246a8977ff572fa8d
Marcel Galliker <marcel.galliker@mouvent.com>
2020-11-11 07:18:03 +0100

    GUI: Added RxCamSettings

---
+++ mvt_digiprint_gui/rx_CamLib/RxCamSettings.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace rx_CamLib
+{
+	public class RxCamSettings
+	{
+		//--- Property DeviceName ---------------------------------------
+		private string _DeviceName;
+		public string DeviceName
+		{
+			get { return _DeviceName; }
+			set { _DeviceName=value; }
+		}
+
+
+
+	}
+}
