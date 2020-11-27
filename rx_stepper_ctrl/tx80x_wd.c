635e62af9721a8ee588cb47cb3b360369ceaaf64
Andre Luescher <Andre.Luescher@mouvent.com>
2020-11-27 15:11:08 +0000

    Merged PR 402: Bugfix in Error Handling of Wrinkle Detection
    fit: Bugfix in Error Handling of Wrinkle Detection (In error message was added an integer, but a string was expected)

--- rx_stepper_ctrl/tx80x_wd.c
+++ rx_stepper_ctrl/tx80x_wd.c
@@ -51,6 +51,7 @@ static void _tx80x_wd_move_to_pos(int cmd, int pos);
 static int  _steps_2_micron(int steps);
 static int  _micron_2_steps(int micron);
 static void _tx80x_wd_motor_z_test(int steps);
+static char *_motor_name(int no);
 
 // global Variables
 static int  _CmdRunning = 0;
@@ -129,13 +130,10 @@ void tx80x_wd_main(void)
         {
             for (motor = MOTOR_WD_FRONT, ok = TRUE; motor < MOTOR_WD_FRONT + MOTOR_WD_CNT; motor++)
             {
-                if ((Fpga.stat->statMot[motor].err_estop & ENC_ESTOP_ENC))
+                if ((Fpga.stat->statMot[motor].err_estop & ENC_ESTOP_ENC) && !fpga_input(motor - MOTOR_WD_FRONT + WD_FRONT_STORED_IN))
                 {
-                    if (!fpga_input(motor))
-                    {
-                        Error(ERR_ABORT, 0, "WRINKLE DETECTION: %s: motor %s blocked", _CmdName, motor + 1);
-                        ok = FALSE;
-                    }
+                    Error(ERR_ABORT, 0, "WRINKLE DETECTION: %s: motor %s blocked", _CmdName, _motor_name(motor));
+                    ok = FALSE;
                 }
             }
             motors_reset(MOTOR_WD_BITS);
@@ -147,7 +145,7 @@ void tx80x_wd_main(void)
             {
                 if (motor_error(motor))
                 {
-                    Error(ERR_ABORT, 0, "WRINKLE DETECTION: %s: motor %s blocked", _CmdName, motor + 1);
+                    Error(ERR_ABORT, 0, "WRINKLE DETECTION: %s: motor %s blocked", _CmdName, _motor_name(motor));
                     ok = FALSE;
                 }
             }
@@ -405,4 +403,22 @@ static void _tx80x_wd_motor_z_test(int steps)
         return TRUE;
     }
     return FALSE;
+}
+
+//--- _motor_name ------------------------------
+static char *_motor_name(int no)
+{
+    switch (no)
+    {
+    case 0:
+        return "Robot Rotation";
+    case 1:
+        return "Robot Shift";
+    case 2:
+        return "WD Front";
+    case 3:
+        return "WD Back";
+    default:
+        return "";
+    }
 }
