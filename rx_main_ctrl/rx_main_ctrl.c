f0950b6bde0beb0019de4df4fae0305617716cf0
Marcel Galliker <marcel.galliker@mouvent.com>
2021-02-25 14:05:32 +0100

    Improved Setup Assist

--- rx_main_ctrl/rx_main_ctrl.c
+++ rx_main_ctrl/rx_main_ctrl.c
@@ -211,7 +211,8 @@ int main(int argc, char* argv[])
 	// ?? bist du sicher, dass nie ein memset(&RX_Config, 0x00, ...) gemacht wird? (es könnte auch ein übergebener Pointer sein)
 	// ?? problem Konzept: Wenn die Screwpos im RX_Config gespeichert sind, sollten sie im setup_config geschrieben/gelesen werden
 	//    -> andere Lösung: RX_Screwpos als eigene Struktur verwalten, evtl. nur als static im step_lb.c
-	
+	//		die Positionen verändern sich nur nach einem Suchen: ich würde am Ende des Suchvorgangs eine Meldung EVT_SAVE_SCREW_POS ans main schicken.	
+
 	ctrl_update_hostname();
 	
 	if (!strcmp(RX_Hostname, "TEST-0001"))
