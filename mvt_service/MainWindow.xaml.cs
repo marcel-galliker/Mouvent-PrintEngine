6926c4498b18b1f81ce7ee93a07d18da9d59356f
Tommy Gerardi <tommy.gerardi@mouvent.com>
2021-05-11 13:21:25 +0000

    Merged PR 708: miss: Release number in mvt_service
    Related work items: #1692

--- mvt_service/MainWindow.xaml.cs
+++ mvt_service/MainWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using System.Timers;
@@ -22,11 +23,10 @@ namespace mvt_service
     /// </summary>
     public partial class MainWindow : Window
     {
-        private const String VERSION_PLACEHOLDER = "1.5";
         private const String MACHINE_NAME_PLACEHOLDER = "Unused";
         private const String PIN_INFO_TEXT = "Actual Pin for version ";
 
-        private readonly MvtUserLevelManager mml = new MvtUserLevelManager(VERSION_PLACEHOLDER, MACHINE_NAME_PLACEHOLDER);
+        private readonly MvtUserLevelManager mml;
         private Timer pinTimer = new Timer(10000);
         private MvtTOTP codes;
 
@@ -34,9 +34,13 @@ namespace mvt_service
         {
             InitializeComponent();
 
+            Version v = Assembly.GetExecutingAssembly().GetName().Version;
+            string version = string.Format("{0}.{1}", v.Major, v.Minor);
+
+            mml = new MvtUserLevelManager(version, MACHINE_NAME_PLACEHOLDER);
             codes = new MvtTOTP(mml.CreateMouventSecret(), true, null);
 
-            PinInfo.Text = PIN_INFO_TEXT + VERSION_PLACEHOLDER;
+            PinInfo.Text = PIN_INFO_TEXT + version;
             PinValue.Text = codes.GetPin();
 
             pinTimer.Elapsed += this.PinRefresh;
