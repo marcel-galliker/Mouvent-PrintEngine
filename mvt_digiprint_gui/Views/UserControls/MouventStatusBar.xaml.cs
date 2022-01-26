73d210b2b084a4315cef58d3489383cd97603ccf
AndreL <andre.luescher@mouvent.com>
2022-01-26 11:04:40 +0100

    Removed possibilty to avoid password for Testtable Seon

--- mvt_digiprint_gui/Views/UserControls/MouventStatusBar.xaml.cs
+++ mvt_digiprint_gui/Views/UserControls/MouventStatusBar.xaml.cs
@@ -167,21 +167,14 @@ namespace RX_DigiPrint.Views.UserControls
 
         private void UserType_Click(object sender, RoutedEventArgs e)
         {
-            if (RxGlobals.PrintSystem.PrinterType == EPrinterType.printer_test_table_seon)
-			{
-                RxGlobals.User.UserType = EUserType.usr_engineer;
-            }
-			else
-			{
-                MvtUserLevelManager ulm = new MvtUserLevelManager(SettingsDlg.GetVersion(), RxGlobals.PrinterProperties.Host_Name);
-                UserLogin ulw = new UserLogin(ulm);
-                ulw.Owner = Window.GetWindow(this);
-                if ((bool)ulw.ShowDialog())
-                {
-                    /* New user type corresponds to the result of the authentication */
-                    RxGlobals.User.UserType = (EUserType)ulm.GetLevel();
-                }
-            }
+           MvtUserLevelManager ulm = new MvtUserLevelManager(SettingsDlg.GetVersion(), RxGlobals.PrinterProperties.Host_Name);
+           UserLogin ulw = new UserLogin(ulm);
+           ulw.Owner = Window.GetWindow(this);
+           if ((bool)ulw.ShowDialog())
+           {
+               /* New user type corresponds to the result of the authentication */
+               RxGlobals.User.UserType = (EUserType)ulm.GetLevel();
+           }
         }
 
         private void Logout_Click(object sender, RoutedEventArgs e)
